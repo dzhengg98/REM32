98ac437d95e37402fee2e3d590d64306a326bc93
David Zheng <dzheng3456@gmail.com>
2022-06-19 01:45:46 -0400

    finish store

--- frontend/rem32.jsx
+++ frontend/rem32.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import configureStore from './store/store';
+
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
+  const store = configureStore();
+
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
+  
   ReactDOM.render(<h1>Welcome to Rem32</h1>, root);
 });
