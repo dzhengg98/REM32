4dcae2732da71fccd2a8dc835c09df7dcfb00969
David Zheng <dzheng3456@gmail.com>
2022-06-19 00:54:01 -0400

    finish session actions

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,37 @@
+import * as sessionAPIUtil from '../util/session_api_util';
+
+export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+
+export const receiveCurrentUser = user => ({
+  type: RECEIVE_CURRENT_USER,
+  user,
+});
+
+export const logoutCurrentUser = () => ({
+  type: LOGOUT_CURRENT_USER,
+})
+
+export const receiveErrors = errors => ({
+  type: RECEIVE_ERRORS,
+  errors,
+});
+
+export const login = user => dispatch => (
+  sessionAPIUtil.login(user)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail(err => dispatch(receiveErrors(err.responseJSON)))
+);
+  
+export const logout = () => dispatch => (
+  sessionAPIUtil.logout()
+    .then(() => dispatch(logoutCurrentUser()))
+    .fail(err => dispatch(receiveErrors(err.responseJSON)))
+);
+
+export const signup = user => dispatch => (
+  sessionAPIUtil.signup(user)
+    .then(user => dispatch(receiveCurrentUser(user)))
+    .fail(err => dispatch(receiveErrors(err.responseJSON)))
+);
