798a713212727d77fb7317148d30be43ce80e861
David Zheng <dzheng3456@gmail.com>
2022-06-19 22:37:37 -0400

    user auth

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import HeaderContainer from "./header/header_container";
+import { Route, Switch } from 'react-router-dom';
+import LoginFormContainer from './session_form/login_form_container';
+import SignupFormContainer from './session_form/signup_form_container';
+import { AuthRoute } from '../util/route_util';
 
 const App = () => (
   <div>
@@ -7,6 +11,10 @@ const App = () => (
       <h1>REM32</h1>
       <HeaderContainer />
     </header>
+    <Switch>
+      <AuthRoute exact path="/login" component={LoginFormContainer} />
+      <AuthRoute exact path="/signup" component={SignupFormContainer} />
+    </Switch>
   </div>
 );
 
