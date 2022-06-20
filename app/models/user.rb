51c6c1e1562e30c4558ac6a72ab3715290da951c
David Zheng <dzheng3456@gmail.com>
2022-06-19 22:02:45 -0400

    minor commits

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   
   validates :username, :session_token, :password_digest, presence: true
   validates :username, uniqueness: true
-  validates :password, length: { minimum: 8, allow_nil: true }
+  validates :password, length: { minimum: 6, allow_nil: true }
   
   attr_reader :password
 
--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -2,7 +2,7 @@ import * as sessionAPIUtil from '../util/session_api_util';
 
 export const RECEIVE_CURRENT_USER = 'RECEIVE_CURRENT_USER';
 export const LOGOUT_CURRENT_USER = 'LOGOUT_CURRENT_USER';
-export const RECEIVE_ERRORS = 'RECEIVE_ERRORS';
+export const RECEIVE_SESSION_ERRORS = 'RECEIVE_SESSION_ERRORS';
 
 export const receiveCurrentUser = user => ({
   type: RECEIVE_CURRENT_USER,
@@ -14,24 +14,23 @@ export const logoutCurrentUser = () => ({
 })
 
 export const receiveErrors = errors => ({
-  type: RECEIVE_ERRORS,
+  type: RECEIVE_SESSION_ERRORS,
   errors,
 });
 
 export const login = user => dispatch => (
   sessionAPIUtil.login(user)
-    .then(user => dispatch(receiveCurrentUser(user)))
-    .fail(err => dispatch(receiveErrors(err.responseJSON)))
+    .then(user => dispatch(receiveCurrentUser(user))), 
+    err => (dispatch(receiveErrors(err.responseJSON)))
 );
   
 export const logout = () => dispatch => (
   sessionAPIUtil.logout()
     .then(() => dispatch(logoutCurrentUser()))
-    .fail(err => dispatch(receiveErrors(err.responseJSON)))
 );
 
 export const signup = user => dispatch => (
   sessionAPIUtil.signup(user)
-    .then(user => dispatch(receiveCurrentUser(user)))
-    .fail(err => dispatch(receiveErrors(err.responseJSON)))
+    .then(user => dispatch(receiveCurrentUser(user))),
+    err => (dispatch(receiveErrors(err.responseJSON)))
 );
