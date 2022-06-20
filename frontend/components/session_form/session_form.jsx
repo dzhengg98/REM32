289591946616392be3964b6401d1f06eead970f9
David Zheng <dzheng3456@gmail.com>
2022-06-20 17:03:06 -0400

    finished styling

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -30,28 +30,28 @@ class SessionForm extends React.Component {
 
   showHeader() {
     return this.props.formType === "login" ? (
-      <h1>Log in to REM32</h1>
+      <h1 className="auth-header">Log in to REM32</h1>
     ) : (
       <div>
-        <h1>Join REM32</h1>
-        <p>Discover and share incredible photos, gain global exposure, and get paid for your work.</p>
+        <h1 className="auth-header">Join REM32</h1>
+        <p className="auth-description">Discover and share incredible photos, gain global exposure, and get paid for your work.</p>
       </div>
     );
   }
 
   renderLink() {
     return this.props.formType === "login" ? (
-      <p class="form-statement">Don't have an account? <Link to="/signup">Sign Up</Link></p>
+      <p className="form-statement">Don't have an account? <Link to="/signup">Sign Up</Link></p>
     ) : (
-      <p class="form-statement">Already have an account? <Link to="/login">Log in</Link></p>
+      <p className="form-statement">Already have an account? <Link to="/login">Log in</Link></p>
     );
   }
 
   renderErrors() {
-    const errs = this.props.errors.map((err, i) => {
-      return <li key={i}>{err}</li>
+    const errors = this.props.errors.map((error, i) => {
+      return <li key={i}>{error}</li>
     })
-    return this.props.errors ? errs : "&nbsp;";
+    return this.props.errors ? errors : "&nbsp;";
   }
 
   render(){
@@ -59,8 +59,10 @@ class SessionForm extends React.Component {
       <div className="session_form">
         <form onSubmit={this.handleSubmit}>
           {this.showHeader()}
-          <label>Username 
+
+          <label className="auth-label">Username 
             <input
+              className="auth-input"
               type="text"
               value={this.state.username}
               onChange={this.update("username")}
@@ -68,20 +70,24 @@ class SessionForm extends React.Component {
             />
           </label>
 
-          <label>Password 
+          <label className="auth-label">Password 
             <input
+              className="auth-input"
               type="password"
               value={this.state.password}
               onChange={this.update("password")}
               placeholder="password"
             />
           </label>
+
           <ul className="error-color">{this.renderErrors()}</ul>
+
           <input
             type="submit"
             className = "button"
             value={this.props.formType === "login" ? "Log in" : "Sign up"}
           />
+
           {this.renderLink()}
         </form>
       </div>
