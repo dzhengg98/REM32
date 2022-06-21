9be671e40b5b9981ecf98822dc93b54d1d1d1558
David Zheng <dzheng3456@gmail.com>
2022-06-21 09:40:49 -0400

    created splash page and some css

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -5,6 +5,7 @@ import HeaderContainer from './header/header_container';
 import login_form_container from './session_form/login_form_container';
 import signup_form_container from './session_form/signup_form_container';
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
+import Splash from './splash_page';
 
 const App = () => (
   <div>
@@ -14,6 +15,7 @@ const App = () => (
     </header>
 
     <Switch>
+      <AuthRoute exact path="/" component={Splash} />
       <AuthRoute path="/login" component={login_form_container} />
       <AuthRoute path="/signup" component={signup_form_container} />
     </Switch>
---
+++ frontend/components/splash_page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Splash = () => {
+  return (
+    <div className="splash-page">
+      <h1 className="splash-title">Discover and share the world's best photos</h1>
+      <p className="splash-description">Get inspired with incredible photos from diverse styles and genres around the world. We're not guided by fads—just great photography.</p>
+      <button className="splash-sign-up-button"><span>Sign up</span></button>
+    </div>
+  )
+}
+
+export default Splash;
