12e6bf57af97970b5e9548cb5d1983ab0a0bc3bc
David Zheng <dzheng3456@gmail.com>
2022-06-25 19:47:18 -0400

    media css work

--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -11,9 +11,9 @@ const Header = props => {
     <div className="header-right">
       {/* <p className="welcome-message">Hello {props.currentUser.username}!</p> */}
       <input type="text" disabled className="header-search" placeholder="Search REM32"/>
-      <h2 className="header-icon"><a href="#"><img src={window.userIcon} /></a></h2>
-      <h2 className="header-icon"><a href="#"><img src={window.messageIcon} /></a></h2>
-      <h2 className="header-icon"><a href="#"><img src={window.notificationIcon} /></a></h2>
+      <h2 className="header-icon-user"><a href="#"><img src={window.userIcon} /></a></h2>
+      <h2 className="header-icon-null"><a href="#"><img src={window.messageIcon} /></a></h2>
+      <h2 className="header-icon-null"><a href="#"><img src={window.notificationIcon} /></a></h2>
       <ul className="user-auth-buttons">
         <li key="logout" className="log-out"><a onClick={logoutUser}>Logout</a></li>
         <li key="upload" className="upload-button"><Link to="/images/new">Upload</Link></li>
