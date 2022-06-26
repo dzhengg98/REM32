4b7dddbda79107060d573b0ac2ca68b5c086a13b
David Zheng <dzheng3456@gmail.com>
2022-06-26 17:53:59 -0400

    added user_api_util

--- frontend/components/Images/image_index_item.jsx
+++ frontend/components/Images/image_index_item.jsx
@@ -6,6 +6,7 @@ const ImageIndexItem = (props) => {
     <div className="image-display-main">
       <Link to={`/images/${props.image.id}`} className="image-link">
         <img src={props.image.imageUrl} className="image-display" />
+        <p className="image-display-title">{props.image.title}</p>
       </Link>
     </div>
   )
