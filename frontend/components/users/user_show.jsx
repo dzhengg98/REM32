933e30a7e6e860d490d28830567c9ad98cb72a2a
David Zheng <dzheng3456@gmail.com>
2022-06-29 16:32:18 -0400

    editing user show -- still works2

--- frontend/components/users/user_show.jsx
+++ frontend/components/users/user_show.jsx
@@ -25,32 +25,14 @@ class UserShow extends React.Component {
     this.props.user ? this.setState({id: this.props.user.id}) : this.setState({id: Number(this.props.match.params.userId)})
   }
 
-  handleProfilePicSubmit() {
-    const formData = new FormData();
+  // COVER PIC
 
-    if (this.state.profilepic) {
-      formData.append('user[profilepic]', this.state.profilepic);
-      formData.append('user[id]', this.state.id);
-    }
-
-    this.props.updateUserProfilePic(formData, this.props.user.id);
-  }
-
-  handleProfilePicFile(e) {
-    e.preventDefault();
-    const file = e.target.files[0];
-    const fileReader = new FileReader();
-
-    fileReader.onloadend = () => {
-      this.setState({
-        profilepic: file,
-        profilepicUrl: fileReader.result,
-      }, () => this.handleProfilePicSubmit());
-    }
-
-    if (file) {
-      fileReader.readAsDataURL(file);
-    }
+  userCoverPic() {
+    return this.props.user.coverpic ? (
+      <img className="user-cover-picture-image" src={this.props.user.coverpic}/>
+    ) : (
+      <div></div>
+    )
   }
 
   handleCoverPicSubmit() {
@@ -78,6 +60,46 @@ class UserShow extends React.Component {
       fileReader.readAsDataURL(file);
     }
   }
+
+  // PROFILE PIC
+
+  handleProfilePicSubmit() {
+    const formData = new FormData();
+
+    if (this.state.profilepic) {
+      formData.append('user[profilepic]', this.state.profilepic);
+      formData.append('user[id]', this.state.id);
+    }
+
+    this.props.updateUserProfilePic(formData, this.props.user.id);
+  }
+
+  userProfilePic() {
+    return this.props.user.profilepic ? (
+      <img className="circular" src={this.props.user.profilepic}/>
+    ) : (
+      <img src={window.userIcon}/>
+    )
+  }
+
+  handleProfilePicFile(e) {
+    e.preventDefault();
+    const file = e.target.files[0];
+    const fileReader = new FileReader();
+
+    fileReader.onloadend = () => {
+      this.setState({
+        profilepic: file,
+        profilepicUrl: fileReader.result,
+      }, () => this.handleProfilePicSubmit());
+    }
+
+    if (file) {
+      fileReader.readAsDataURL(file);
+    }
+  }
+
+  // PROFILE LOCATION
 
   userProfileLocation() {
     return (this.props.user.city && this.props.user.country) ? (
@@ -111,6 +133,8 @@ class UserShow extends React.Component {
     )
   }
 
+  // PROFILE ABOUT
+
   userProfileAbout() {
     return this.props.user.about ? (
       <div className={`${this.props.currentUser.id === this.props.user.id ? ("user-profile-about") : ("other-user-about")}`}>{this.props.user.about}</div>
@@ -118,6 +142,8 @@ class UserShow extends React.Component {
       <div></div>
     )
   }
+
+  // PROFILE TWITTER LINK
 
   userTwitterLink() {
     return this.props.user.twitter ? (
@@ -131,6 +157,8 @@ class UserShow extends React.Component {
     )
   }
 
+  // PROFILE INSTAGRAM LINK
+
   userInstagramLink() {
     return this.props.user.instagram ? (
       <div className={`${this.props.currentUser.id === this.props.user.id ? ("user-profile-instagram-link") : ("other-user-profile-instagram-link")}`}>
@@ -142,6 +170,8 @@ class UserShow extends React.Component {
       <div></div>
     )
   }
+
+  // PROFILE FACEBOOK LINK
 
   userFacebookLink() {
     return this.props.user.facebook ? (
@@ -155,6 +185,8 @@ class UserShow extends React.Component {
     )
   }
 
+  // PROFILE WEBSITE LINK
+
   userWebsiteLink() {
     return this.props.user.website ? (
       <div className={`${this.props.currentUser.id === this.props.user.id ? ("user-profile-website-link") : ("other-user-profile-website-link")}`}>
@@ -162,22 +194,6 @@ class UserShow extends React.Component {
           <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg" fillRule="evenodd" clipRule="evenodd"><path d="M12.02 0c6.614.011 11.98 5.383 11.98 12 0 6.623-5.376 12-12 12-6.623 0-12-5.377-12-12 0-6.617 5.367-11.989 11.981-12h.039zm3.694 16h-7.427c.639 4.266 2.242 7 3.713 7 1.472 0 3.075-2.734 3.714-7m6.535 0h-5.523c-.426 2.985-1.321 5.402-2.485 6.771 3.669-.76 6.671-3.35 8.008-6.771m-14.974 0h-5.524c1.338 3.421 4.34 6.011 8.009 6.771-1.164-1.369-2.059-3.786-2.485-6.771m-.123-7h-5.736c-.331 1.166-.741 3.389 0 6h5.736c-.188-1.814-.215-3.925 0-6m8.691 0h-7.685c-.195 1.8-.225 3.927 0 6h7.685c.196-1.811.224-3.93 0-6m6.742 0h-5.736c.062.592.308 3.019 0 6h5.736c.741-2.612.331-4.835 0-6m-12.825-7.771c-3.669.76-6.671 3.35-8.009 6.771h5.524c.426-2.985 1.321-5.403 2.485-6.771m5.954 6.771c-.639-4.266-2.242-7-3.714-7-1.471 0-3.074 2.734-3.713 7h7.427zm-1.473-6.771c1.164 1.368 2.059 3.786 2.485 6.771h5.523c-1.337-3.421-4.339-6.011-8.008-6.771"/></svg>
         </a>
       </div>
-    ) : (
-      <div></div>
-    )
-  }
-
-  userProfilePic() {
-    return this.props.user.profilepic ? (
-      <img className="circular" src={this.props.user.profilepic}/>
-    ) : (
-      <img src={window.userIcon}/>
-    )
-  }
-
-  userCoverPic() {
-    return this.props.user.coverpic ? (
-      <img className="user-cover-picture-image" src={this.props.user.coverpic}/>
     ) : (
       <div></div>
     )
