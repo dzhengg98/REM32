6a37c3bcbdedbd40e76be1d73314368663457ec8
David Zheng <dzheng3456@gmail.com>
2022-06-30 22:20:16 -0400

    changed some css around

--- frontend/components/splash/developer_message.jsx
+++ frontend/components/splash/developer_message.jsx
@@ -2,13 +2,27 @@ import React from 'react';
 
 const DeveloperMessage = () => {
   return (
-    <main className="message-board">
-      <section className="message-section">
-        <div className="message-info">
-          <h2 className="message-board-header">Welcome to REM32</h2>
-          <p className="message-board-paragraph">REM32 is a clone of the popular photosharing website <a href="#">500px.</a></p>
-          <p className="message-board-paragraph">Feel free to browse around, view profiles and add some comments!</p>
+    <main className="message-board">     
+      <div className="links-container">
+        <div className="links">
+          <button className="button-links"><a href="https://github.com/dzhengg98">Github</a></button>
+          <button className="button-links"><a href="https://www.linkedin.com/in/david-zheng-87671b237/">Linkedin</a></button>
+          <button className="button-links"><a href="https://github.com/dzhengg98/rem32/wiki">Project Page</a></button>
         </div>
+      </div>
+      <div className="message-info-container">
+        <div className="message-info-inner">
+          <div className="message-info-header">
+            <h2>Welcome to REM32</h2>
+          </div>
+          <div className="message-info-para">
+            <p className="message-board-paragraph">REM32 is a clone of the popular photosharing website: <a href={`//${"www.500px.com"}`} target="_blank" rel="noopener noreferrer">500px.</a></p>
+            <p className="message-board-paragraph">Made By: David Zheng</p>
+            <p className="message-board-paragraph">Feel free to browse around, view profiles and add some comments!</p>
+          </div>
+        </div>
+      </div>
+      <section className="message-section">
         <div className="leaf-set">
           <div className="leaf-1">
             <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 24 24"><path d="M15.787 7.531c-5.107 2.785-12.72 9.177-15.787 15.469h2.939c.819-2.021 2.522-4.536 3.851-5.902 8.386 3.747 17.21-2.775 17.21-11.343 0-1.535-.302-3.136-.92-4.755-2.347 3.119-5.647 1.052-10.851 1.625-7.657.844-11.162 6.797-8.764 11.54 3.506-3.415 9.523-6.38 12.322-6.634z"/></svg>
