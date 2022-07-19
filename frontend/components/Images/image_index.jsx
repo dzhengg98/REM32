f1214f4247650820429afe339b09c7896f238b45
David Zheng <dzheng3456@gmail.com>
2022-07-19 12:33:21 -0400

    loading indicator works and user show changed to functional

--- frontend/components/Images/image_index.jsx
+++ frontend/components/Images/image_index.jsx
@@ -1,19 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ImageIndexItem from './image_index_item';
+import sleep from '../../util/sleep';
 
 const ImageIndex = (props) => {
 
   const { fetchImages, fetchUsers, images } = props;
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    fetchImages();
+    const getImages = async () => {
+      setIsLoading(true);
+      const res = await fetchImages();
+      await sleep(1200);
+      setIsLoading(false);
+    }
+    getImages();
     fetchUsers();
   }, []);
 
   return (
     <div className="images-gallery-container">
-      {
+      {isLoading ? (
+        <div className="loading-indicator-container">
+          <svg className="spinner" viewBox="0 0 50 50">
+            <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
+          </svg>
+        </div>
+      ) : (
         images.reverse().map((image) => <ImageIndexItem key={image.id} image={image} />)
+      )
       }
     </div>
   )
